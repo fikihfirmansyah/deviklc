29f8aa3f20284a76aed43baa81d8dc2bb89d2906
fikihfirmansyah <fikihfirmansyah43@gmail.com>
2021-08-29 02:02:50 +0700

    welcome

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+
+<head>
+    <!-- Your Website Title -->
+    <title>dev. IKLC USU</title>
+    <!-- Your Website Description -->
+    <meta name="description" content="dev. IKLC USU " />
+    <meta name="author" content="dev. IKLC USU">
+
+    <!-- Meta Tags for Responsiveness -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Social Media Meta Tags -->
+    <meta property="og:url" content="http://iklc.or.id/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="dev. IKLC USU" />
+    <meta property="og:description" content="dev. IKLC USU" />
+    <meta property="og:image" content="assets/img/iklc.png" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/iklcwhite.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ site.baseurl }}public/apple-touch-icon-144-precomposed.png">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,600,700,300i,600i,700i" rel="stylesheet">
+
+    <!-- Bootstrap & Other CSS Files -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" integrity="sha512-+EoPw+Fiwh6eSeRK7zwIKG2MA8i3rV/DGa3tdttQGgWyatG/SkncT53KHQaS5Jh9MNOT3dmFL0FjTY08And/Cw==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css">
+
+    <!-- Project CSS File -->
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+    <!-- Font Awesome -->
+
+    <script src="https://kit.fontawesome.com/400db8db7c.js" crossorigin="anonymous"></script>
+</head>
+
+<body onclick="colourChange()">
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+        <!-- Header -->
+        <header>
+            <div class="logo animate">
+                <a href="http://dev.iklc.or.id">
+                    <img class="img-responsive" width="80%" height="auto" src="assets/img/iklc.png" alt="iklc">
+                </a>
+            </div>
+        </header>
+
+        <!-- Content Wrapper -->
+        <div class="content-wrapper">
+            <div class="content-main animate">
+                <div class="container">
+                    <h1 class="page-title">dev. IKLC USU</h1>
+                    <div class="wrapper-text">
+                        <!-- <h3 >I'm -->
+                        <h3>
+                            <span class="txt-rotate" data-period="2000" data-rotate='[ "Algoritma & Pemrograman.", "Analisis Numerik.", "Desain & Analisis Algoritma.", "Kecerdasan Buatan dan Sistem Cerdas.", "Komputer Grafik dan Visualisasi.", "Komunikasi Data dan Jaringan Komputer.", "Konsep Dasar Pemrograman.", "Manajemen Proyek Informatika.", "Mikroprosessor dan Assembly.", "Mobile Programming.", "Organisasi dan Arsitektur Komputer.", "Pemrograman Berorientasi Objek.", "Pemrograman Visual.", "Pemrograman Web 2.", "Pemrograman Web I.", "Pengajaran Berbantuan Komputer.", "Pengantar Ilmu Komputer.", "Pengolahan Citra.", "Probabilitas & Statistika.", "Rekayasa Perangkat Lunak.", "Sistem Database Lanjutan.", "Sistem Digital.", "Sistem Informasi.", "Sistem Manajemen Database.", "Stuktur Data dan Algoritma.", "Teknik Kompilasi."]'>
+                            </span>
+                        </h3>
+                        <!-- </h3> -->
+                        <!-- <p class="flipText">Android Developer 👨‍💻</p> -->
+                    </div>
+                </div>
+            </div>
+            <!-- Background Wrapper -->
+            <div class="background-wrapper">
+                <div id="background-content"></div>
+            </div>
+        </div>
+
+        <!-- Bootstrap Scripts -->
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js" integrity="sha512-UdIMMlVx0HEynClOIFSyOrPggomfhBKJE28LKl8yR3ghkgugPnG6iLfRfHwushZl1MOPSY6TsuBDGPK2X4zYKg==" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js" integrity="sha512-IsNh5E3eYy3tr/JiX2Yx4vsCujtkhwl7SLqgnwLNgf04Hrt9BT9SXlLlZlWx+OK4ndzAoALhsMNcCmkggjZB1w==" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+        <!-- Sketch.js -->
+        <script src="assets/js/sketch.min.js"></script>
+
+        <!-- Project JS File -->
+        <script src="assets/js/script.js"></script>
+
+</body>
+
+</html>
